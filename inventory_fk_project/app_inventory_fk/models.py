18557647619ea2b60f7cc1287739005a1fdda744
momo <morganfacorat@gmail.com>
2020-12-09 10:00:51 +0100

    INIT MODEL
    INIT MODEL

--- inventory_fk_project/app_inventory_fk/models.py
+++ inventory_fk_project/app_inventory_fk/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Marque(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    url = models.URLField()
+    supplier_info = models.TextField(null=True)
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    img_url = models.URLField()
+    marque = models.ForeignKey(Marque, on_delete=models.CASCADE, null=True)
+
+
+class Department (models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    location = models.TextField(null=True)
+    info = models.TextField(null=True)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    model = models.CharField(max_length=200, unique=True)
+    note_info = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
+
+
