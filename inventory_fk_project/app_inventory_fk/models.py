e10bb30ac6c9f95e0a78f62599e0f17a5dfe7433
momo <morganfacorat@gmail.com>
2020-12-09 10:17:22 +0100

    UPDATE MODEL
    UPDATE MODEL

--- inventory_fk_project/app_inventory_fk/models.py
+++ inventory_fk_project/app_inventory_fk/models.py
@@ -4,27 +4,32 @@ from django.db import models
 class Marque(models.Model):
     name = models.CharField(max_length=200, unique=True)
     url = models.URLField()
-    supplier_info = models.TextField(null=True)
+    logo_img_url = models.URLField()
+    optional_info = models.TextField(null=True)
 
 
 class Type(models.Model):
     name = models.CharField(max_length=200, unique=True)
+
+
+class ModelItem(models.Model):
+    name = models.CharField(max_length=200, unique=True)
     img_url = models.URLField()
     marque = models.ForeignKey(Marque, on_delete=models.CASCADE, null=True)
+    optional_info = models.TextField(null=True)
 
 
 class Department (models.Model):
     name = models.CharField(max_length=200, unique=True)
-    location = models.TextField(null=True)
-    info = models.TextField(null=True)
+    optional_info = models.TextField(null=True)
 
 
 class Item(models.Model):
     name = models.CharField(max_length=200, unique=True)
     model = models.CharField(max_length=200, unique=True)
-    note_info = models.TextField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
     type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
+    optional_info = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
